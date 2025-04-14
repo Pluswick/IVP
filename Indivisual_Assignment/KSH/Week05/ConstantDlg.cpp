af3eb39d60fe44dc8c7b2c0358085ef78a9b1d6f
Pluswick <tjdgks0814@gmail.com>
2025-04-14 18:31:41 +0900

    upload files
    for week05's assignment

---
+++ Indivisual_Assignment/KSH/Week05/ConstantDlg.cpp
@@ -0,0 +1,40 @@
+﻿// ConstantDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "MFCApplication_IPtest.h"
+#include "afxdialogex.h"
+#include "ConstantDlg.h"
+
+
+// ConstantDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(ConstantDlg, CDialog)
+
+ConstantDlg::ConstantDlg(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DIALOG4, pParent)
+	, m_StartPoint(0)
+	, m_EndPoint(0)
+{
+
+}
+
+ConstantDlg::~ConstantDlg()
+{
+}
+
+void ConstantDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, m_StartPoint);
+	DDV_MinMaxInt(pDX, m_StartPoint, 0, 255);
+	DDX_Text(pDX, IDC_EDIT2, m_EndPoint);
+	DDV_MinMaxInt(pDX, m_EndPoint, 0, 255);
+}
+
+
+BEGIN_MESSAGE_MAP(ConstantDlg, CDialog)
+END_MESSAGE_MAP()
+
+
+// ConstantDlg 메시지 처리기
---
+++ Indivisual_Assignment/KSH/Week05/ConstantDlg.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// ConstantDlg 대화 상자
+
+class ConstantDlg : public CDialog
+{
+	DECLARE_DYNAMIC(ConstantDlg)
+
+public:
+	ConstantDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~ConstantDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG4 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	int m_StartPoint;
+	int m_EndPoint;
+};
