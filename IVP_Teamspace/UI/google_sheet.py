1c320cbe2e366628758e730b262bcba3951dd093
Seungchan Baek <88337917+kairos1228@users.noreply.github.com>
2025-06-17 23:54:05 +0900

    구글시트 이름 변경

--- IVP_Teamspace/UI/google_sheet.py
+++ IVP_Teamspace/UI/google_sheet.py
@@ -7,7 +7,7 @@ scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/au
 cred_path = os.path.join(os.path.dirname(__file__), "credentials.json")
 creds = ServiceAccountCredentials.from_json_keyfile_name(cred_path, scope)
 client = gspread.authorize(creds)
-worksheet = client.open("CatchCrime_방문기록").worksheet("감지기록")
+worksheet = client.open("[Door-Box] 방문 기록").worksheet("감지기록")
 
 def log_to_google_sheet(time, emotion, gender, age, warning_prefix):
     row = [time, emotion, gender, age, warning_prefix]
